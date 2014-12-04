d474447464d2c370fea460d6d1defad8d2af200c
Akim Boyko <akim.boyko@gmail.com>
2014-12-04 17:59:36 +0200

    Add `EventStream` event

---
+++ samples/06_EventStream.fsx
@@ -0,0 +1,38 @@
+﻿#time "on"
+#load "Bootstrap.fsx"
+
+open System
+open Akka.Actor
+open Akka.Configuration
+open Akka.FSharp
+open Akka.TestKit
+
+// #Event Bus
+// Originally conceived as a way to send messages to groups of actors, the EventBus has been generalized to Event Stream
+// #Event Stream
+// The event stream is the main event bus of each actor system: it is used for carrying log messages and Dead Letters and may be used by the user code for other purposes as well. It uses Subchannel Classification which enables registering to related sets of channels
+
+let system = ActorSystem.Create("FSharp")
+
+let echoServer = 
+    spawn system "EchoServer"
+    <| fun mailbox ->
+        let rec loop() =
+            actor {
+                let! message = mailbox.Receive()
+                match box message with
+                | :? string -> 
+                    printfn "Echo '%s'" message
+                    return! loop()
+                | _ ->  failwith "unknown message"
+            } 
+        loop()
+
+let eventStream = system.EventStream
+
+eventStream.Subscribe(echoServer, typedefof<string>)
+
+eventStream.Publish("Anybody home?")
+eventStream.Publish("Knock knock")
+
+system.Shutdown()
