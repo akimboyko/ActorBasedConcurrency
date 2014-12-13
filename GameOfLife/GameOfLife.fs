e2ba7db7585e6314a5356202a634bf8e592444ec
Akim Boyko <akim.boyko@gmail.com>
2014-12-13 18:42:27 +0200

    ping-pong initial setup

---
+++ GameOfLife/GameOfLife.fs
@@ -0,0 +1,25 @@
+﻿namespace GameOfLife
+
+module Main =
+    open System
+    open Akka.Actor
+    open Akka.Configuration
+    open Akka.FSharp
+
+    let cellActorCont = 
+        let cont (mailbox : Actor<obj>) =
+            let rec loop() =
+                actor { 
+                    let! message = mailbox.Receive()
+                    let sender = mailbox.Sender()
+
+                    match message with
+                    | :? string as msg when msg = "ping" -> 
+                            sender <! "pong"
+                            return! loop()
+                    | _ ->  failwith "unknown message"
+                }
+
+            loop()
+
+        cont
---
+++ GameOfLife/GameOfLifeTests.fs
@@ -0,0 +1,38 @@
+﻿namespace GameOfLife.Tests
+
+module Tests =
+    open Xunit
+    open FsUnit.Xunit
+    open Akka.Actor
+    open Akka.Configuration
+    open Akka.FSharp
+    open Akka.TestKit
+    open Akka.TestKit.Xunit
+    open GameOfLife.Main
+
+    type GameOfLifeTests () as kit =
+        inherit TestKit()
+
+        let system = kit.Sys
+        let timeoutInMs = 1000
+
+        [<Fact>]
+        member self.``Let's play single iteration of ping-pong`` () =
+            let cellRef = spawn system "TestKit" <| cellActorCont
+
+            let future = cellRef <? "ping"
+
+            let reply = Async.RunSynchronously(future, timeoutInMs)
+
+            reply |> should equal "pong"
+            
+        [<Fact>]
+        member self.``Let's play 100 iterations of ping-pong`` () =
+            let cellRef = spawn system "TestKit" <| cellActorCont
+
+            for _ in [1..100] do
+                let future = cellRef <? "ping"
+
+                let reply = Async.RunSynchronously(future, timeoutInMs)
+
+                reply |> should equal "pong"
